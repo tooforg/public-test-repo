5d08fcadcc282cbd46e1cb48866655cf7b34cb20
toofikz1 <toofikz@wearehackerone.com>
2026-06-24 01:32:20 +0200

    add server

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const cp = require('child_process');
+http.createServer((req,res)=>{
+  const u = new URL(req.url,'http://x');
+  const name = u.searchParams.get('name');   // user input
+  eval(name);                                 // CodeQL: js/code-injection
+  cp.exec('echo '+name);                      // CodeQL: js/command-line-injection
+  res.end('ok');
+}).listen(3000);
